e46e4871381d37d6204d8291e8b357e0d02b28cb
Irek Tursunov <79156976+IrekTursunov@users.noreply.github.com>
2026-02-27 18:07:20 +0000

    scripts update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Coding
-print('Hello world!')
+
--- algoritmika/time_and_random_modules.py
+++ algoritmika/time_and_random_modules.py
@@ -12,7 +12,7 @@ t2 = time()
 if txt1 == txt:
     print('Красавчик, все верно!')
     y = len(txt)
-    velocity = y/(t2-t1)*60
+    velocity = *60(y//(t2-t1))
     print('y = ', y, 't2-t1 = ', t2-t1)
     print(velocity, ' - скорость вашей печати')
 else:
