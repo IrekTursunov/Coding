71a0120bc761cfc836fd2f07e37bd0db24c8436d
Irek Tursunov <79156976+IrekTursunov@users.noreply.github.com>
2026-02-27 22:58:55 +0500

    Create time_and_random_modules.py

---
+++ time_and_random_modules.py
@@ -0,0 +1,19 @@
+from random import randint
+from time import time
+x = randint(1, 2)
+if x == 1:
+    txt = 'солнце'
+elif x==2:
+    txt = 'амфибрахий'
+print('Напишите данный текст: ', txt)
+t1 = time()
+txt1 = input()
+t2 = time()
+if txt1 == txt:
+    print('Красавчик, все верно!')
+    y = len(txt)
+    velocity = y/(t2-t1)*60
+    print('y = ', y, 't2-t1 = ', t2-t1)
+    print(velocity, ' - скорость вашей печати')
+else:
+    print('Неправильно :(')
